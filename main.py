93c6adfd07d0bd7ea7bd696a31028e3e43a0844e
Jose Eduardo O. T <rotjeot@gmail.com>
2020-06-12 00:12:20 -0500

    Creación de la funcion para el ordenamiento y para crear un arreglo con datos que el usuario ingresa

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-numbers = [76,43,75,22,74,9,6,24,56,22,54,12,87,44]
+
+def Bubble_Sort(list):
+    numbers = list
+    length = len(numbers)
+    for i in range(length -1, 0, -1):
+        for j in range(0,i):
+            if(numbers[j] > numbers[j+1]):
+                temp = numbers[j]
+                numbers[j] = numbers[j+1]
+                numbers[j+1] = temp
+    return numbers
+
+numbers=[]
+print("Sort of a list with Bubble sort")
+x = int(input("How many numbers do you want insert?"))
+for i in range(x):
+    numbers.append(int(input(f"ingrese elemento, {i+1}: ")))
+
+numbers_sort = Bubble_Sort(numbers)
+print("Tu arreglo ordenado es: ")
+print(numbers_sort)
